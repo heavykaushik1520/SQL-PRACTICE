4c7daa42a9db8da0bd813f642be17367356b6469
heavykaushik1520 <kaushikmandavkar6@gmail.com>
2024-07-31 01:26:56 +0530

    worked : 31-07

---
+++ storeProcedure.sql
@@ -0,0 +1,24 @@
+use t201;
+select * from student;
+
+call getAllStudents();
+
+show procedure status where db="t201";
+
+call getStudentCount();
+
+call getStudentById(1);
+
+call returnStudentCount(@count);
+
+select @count;
+
+call inAndOut('pune' , @count);
+
+select @count ;
+
+call conditionalSP(2);
+
+call loopSP();
+
+show tables;
