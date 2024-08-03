5d872760faeceaf6e2b982529a4be74e64d7bfa1
heavykaushik1520 <kaushikmandavkar6@gmail.com>
2024-08-03 23:53:13 +0530

    views practice

--- views_practice.sql
+++ views_practice.sql
@@ -2,3 +2,23 @@ show databases;
 use practice;
 
 show tables;
+
+select * from products;
+
+desc products;
+
+-- 1 View: Product Names and Prices
+create view BasicProductInfo
+as 
+select ProductName , Price 
+from products;
+
+CREATE VIEW BasicProductInfoo AS
+SELECT ProductName, Price
+FROM Products;
+
+
+select * from BasicProductInfoo;
+
+SELECT ProductName, Price
+FROM Products;
